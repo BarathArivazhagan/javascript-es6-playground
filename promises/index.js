b59dd66a38416f02b7812f202c0215415a9e6c6e
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-04-29 13:25:47 +0530

    code commit

--- promises/index.js
+++ promises/index.js
@@ -1,7 +1,16 @@
 
 import UserService from './UserService';
 
-const users= [{firstName: 'barath', lastName : 'arivazhagan'}];
+const users= [{firstName: 'barath', lastName : 'arivazhagan'},
+    {firstName: 'senthil', lastName : 'anaswamy'},
+    {firstName: 'pari', lastName : 'devi'},
+    {firstName: 'hema', lastName : 'arivazhagan'},
+    {firstName: 'arivazhagan', lastName : 'subramanium'},
+    {firstName: 'kavitha', lastName : 'stephen'},
+    {firstName: 'stephen', lastName : 'balraj'},
+    {firstName: 'meghna', lastName : 'stephen'},
+    {firstName: 'arun', lastName : 'rocks'},
+    {firstName: 'jan', lastName : 'raghu'}];
 
 const userService = new UserService(users);
 
@@ -11,12 +20,16 @@ const promise1= new Promise( (resolve,reject) => {
        resolve(userService.getUsers());
    }else{
        reject(new Error("error in resolving the users"));
-   }
+   }users
 
 
 });
 
-promise1.then(value => {
-    console.log(value);
+promise1.then(users => {
+    console.log(users);
+    let names=users.map( usr => {
+        console.log("called "+usr);
+        return usr.firstName+' '+usr.lastName});
+   console.log("names "+names);
 });
 
